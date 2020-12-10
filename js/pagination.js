1afc3603ba6ee7ba1b79b77ead4fd84e18eade3d
almazesh <eshimbekovalmaz.ib.1.19@gmail.com>
2020-12-10 12:48:56 +0600

    Add the file of pagination.js

---
+++ js/pagination.js
@@ -0,0 +1,62 @@
+const currentPage = document.querySelector('#currentPage');
+const prev = document.querySelector('#prev');
+let pageCounter = 0;
+let dataCount;
+
+fetch(`http://neobisfms.herokuapp.com/api/transactions/?limit=2&offset=${pageCounter}`)
+.then(content => content.json())
+.then(r=>{
+    console.log(r);
+    dataCount = r.count;
+    let frag = '';
+    r["results"].forEach(item=>{
+        let card = cardTemplate(item);
+        frag += card;
+        document.querySelector('.tbody').innerHTML = frag;
+    })
+})
+///////////////////////////    prev
+
+prev.addEventListener('click', l=>{
+    if(pageCounter >= 2){
+        pageCounter -= 2;
+        currentPage.innerHTML = +currentPage.innerHTML - 1;
+    }else{
+        pageCounter = 0;
+    }
+    l.preventDefault();
+    fetch(`http://neobisfms.herokuapp.com/api/transactions/?limit=2&offset=${pageCounter}`)
+    .then(p => p.json())
+    .then(g =>{
+        console.log(g);
+        let frag = '';
+        g["results"].forEach(item=>{
+        let card = cardTemplate(item);
+        frag += card;
+        document.querySelector('.tbody').innerHTML = frag;
+        })
+    })
+})
+///////////////next
+const next = document.querySelector('#next');
+next.addEventListener('click',j=>{
+    if(pageCounter <= dataCount - 2){
+        pageCounter +=2;
+        if(pageCounter <= dataCount -1){	
+            currentPage.innerHTML = +currentPage.innerHTML + 1;
+        }
+    }else{
+        pageCounter = dataCount - 1;
+    }
+    j.preventDefault();
+    fetch(`http://neobisfms.herokuapp.com/api/transactions/?limit=2&offset=${pageCounter}`)
+    .then(u => u.json())
+    .then(h =>{
+        let temp = '';
+        h['results'].forEach(y=>{
+            let card = cardTemplate(y);
+            temp += card;
+            document.querySelector('.tbody').innerHTML = temp;
+        })
+    })
+})
--- js/script.js
+++ js/script.js
@@ -4,67 +4,7 @@ function Toggle() {
    window.addEventListener('click',er=>{
        er.preventDefault()
    })
-   const currentPage = document.querySelector('#currentPage');
-   const prev = document.querySelector('#prev');
-   let pageCounter = 0;
-   let dataCount;
-   
-   fetch(`http://neobisfms.herokuapp.com/api/transactions/?limit=2&offset=${pageCounter}`)
-   .then(content => content.json())
-   .then(r=>{
-       console.log(r);
-       dataCount = r.count;
-       let frag = '';
-       r["results"].forEach(item=>{
-           let card = cardTemplate(item);
-           frag += card;
-           document.querySelector('.tbody').innerHTML = frag;
-       })
-   })
-   
-   prev.addEventListener('click', l=>{
-       if(pageCounter >= 2){
-           pageCounter -= 2;
-           currentPage.innerHTML = +currentPage.innerHTML - 1;
-       }else{
-           pageCounter = 0;
-       }
-       l.preventDefault();
-       fetch(`http://neobisfms.herokuapp.com/api/transactions/?limit=2&offset=${pageCounter}`)
-       .then(p => p.json())
-       .then(g =>{
-           console.log(g);
-           let frag = '';
-           g["results"].forEach(item=>{
-           let card = cardTemplate(item);
-           frag += card;
-           document.querySelector('.tbody').innerHTML = frag;
-           })
-       })
-   })
-        
-   const next = document.querySelector('#next');
-   next.addEventListener('click',j=>{
-       if(pageCounter <= dataCount - 2){
-           pageCounter +=2;
-           if(pageCounter <= dataCount -1){	
-               currentPage.innerHTML = +currentPage.innerHTML + 1;
-           }
-       }else{
-           pageCounter = dataCount - 1;
-       }
-       j.preventDefault();
-       fetch(`http://neobisfms.herokuapp.com/api/transactions/?limit=2&offset=${pageCounter}`)
-       .then(u => u.json())
-       .then(h =>{
-           let temp = '';
-           h['results'].forEach(y=>{
-               let card = cardTemplate(y);
-               temp += card;
-               document.querySelector('.tbody').innerHTML = temp;
-           })
-       })
-   })
+  
    function cardTemplate(card){
    if(card.counterparty == null){
        card.counterparty = '-'
@@ -103,7 +43,7 @@ function Toggle() {
    `
    }
    
-   
+   ///////////total
    fetch('http://neobisfms.herokuapp.com/api/gettotal/')
    .then(tr=> tr.json())
    .then(t =>{
